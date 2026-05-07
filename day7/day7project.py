b55684467ab7c8e12348edf3eda54ce808e228b8
grrrgoT <goterauchi5@gmail.com>
2026-05-07 13:40:01 +0900

    hangman game

---
+++ day7/day7project.py
@@ -0,0 +1,52 @@
+import random
+from hangman_arts import logo, stages
+from hangman_words import word_list
+
+lives = 6
+
+print(logo)
+chosen_word = random.choice(word_list)
+print(chosen_word)
+
+placeholder = ""
+for position in range(0, len(chosen_word)):
+    placeholder += "_"
+print("Word to guess: " + placeholder)
+
+gameover = False
+correct_letters = []
+
+while not gameover:
+    print("****************************{lives}/6 LIVES LEFT****************************")
+    guess = input("Guess a letter: ").lower()
+
+    if guess in correct_letters:
+        print(f"You've already guessed {guess}")
+
+    display = ""
+    for letter in chosen_word:
+        if letter == guess:
+            display += letter
+            correct_letters.append(guess)
+        elif letter in correct_letters:
+            display += letter
+        else:
+            display += "_"
+
+    print("Word to guess: " + display)
+
+    if guess not in chosen_word:
+        lives -= 1
+        print(f"You guessed {guess}, that's not in the word. You lose a life.")
+
+        if lives == 0:
+            gameover = True
+
+        print(f"***********************IT WAS {chosen_word}! YOU LOSE**********************")
+
+    if "_" not in display:
+        gameover = True
+        print("****************************YOU WIN****************************")
+
+    print(stages[lives])
+
